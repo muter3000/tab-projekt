5254b0b3391e7a04a605d47387777e167ad975dd
muter3000 <muter3000@gmail.com>
2022-04-10 15:28:30 +0200

    Docker compose update for backend image building

--- backend/pool.js
+++ backend/pool.js
@@ -5,13 +5,15 @@ class Pool {
         throw new Error('Use Singleton.getInstance()');
     }
     static getInstance() {
+        const host = process.env.PSQL_HOST ?? localhost
+        const port = process.env.PSQL_PORT ?? 30050
         if (!Pool.instance) {
             Pool.instance = new PostgresPool({
                 user: "admin",
                 password: "admin2137",
                 database: "postgres",
-                host: "localhost",
-                port: 30050
+                host: host,
+                port: port
             })
         }
         return Pool.instance;
