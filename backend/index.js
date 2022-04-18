acb62ed2d3980521514f284b501fb8206a6552eb
szebestik@gmail.com <szebestik@gmail.com>
2022-04-18 14:51:40 +0200

    Added CRUD for kategoria_prawa_jazdy table

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,12 @@ app.use(cors({
 const kierowcy = require('./routes/kierowcy')
 const stanowisko_administracyjne = require('./routes/stanowisko_administracyjne')
 const administracja = require('./routes/administracja')
+const kategoria_prawa_jazdy = require('./routes/kategoria_prawa_jazdy')
 
 app.use('/kierowcy', kierowcy);
 app.use('/stanowisko_administracyjne', stanowisko_administracyjne);
 app.use('/administracja', administracja);
+app.use('/kategoria_prawa_jazdy', kategoria_prawa_jazdy);
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
---
+++ backend/routes/kategoria_prawa_jazdy.js
@@ -0,0 +1,119 @@
+const express = require('express')
+const pool = require('../pool')
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const allCategories = await pool.getInstance().query('SELECT * FROM kategoria_prawa_jazdy')
+        
+        return res.status(200).json(allCategories.rows)
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    
+    if (typeof id !== 'number') return res.sendStatus(400)
+
+    try {
+        const specificCategory = await pool.getInstance().query('SELECT * FROM kategoria_prawa_jazdy WHERE id = $1', [id])
+
+        if (specificCategory.rows.length === 0) return res.sendStatus(404)
+        
+        return res.status(200).json(specificCategory.rows[0])
+    }
+    catch(err) {
+        console.log(err)
+        return res.sendStatus(500)
+    }
+})
+
+router.post('/', async (req, res) => {
+    const { kategoria } = req.body
+
+    if (!typ) return res.sendStatus(400)
+
+    try {
+        const addedCategory = await pool.getInstance().query('INSERT INTO kategoria_prawa_jazdy (kategoria) VALUES ($1) RETURNING *', 
+        [kategoria])
+        
+        return res.status(200).json(addedCategory.rows[0])
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+
+    if (typeof id !== 'number') return res.sendStatus(400)
+
+    try {
+        const deletedCategory = await pool.getInstance().query('DELETE FROM kategoria_prawa_jazdy WHERE id = $1 RETURNING *', [id])
+
+        if (deletedCategory.rows.length === 0) return res.sendStatus(404)
+
+        return res.status(200).json(deletedCategory.rows[0])
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500)
+    }
+})
+
+router.put('/', async (req, res) => {
+    const { id, kategoria } = req.body
+
+    if (!id || !kategoria) return res.sendStatus(400)
+
+    try {
+        const specificCategory = typeof id === 'number' ?
+            await pool.getInstance().query('SELECT * FROM kategoria_prawa_jazdy WHERE id = $1', [id]) :
+            {rows: []}
+        let putCategory
+
+        if (specificCategory.rows.length === 0) {
+            putCategory = await pool.getInstance().query('INSERT INTO kategoria_prawa_jazdy (kategoria) VALUES ($1) RETURNING *', 
+            [kategoria])
+        }
+        else {
+            putCategory = await pool.getInstance().query('UPDATE kategoria_prawa_jazdy SET kategoria = $1 WHERE id = $2 RETURNING *',
+            [kategoria, id])
+        }
+        
+        return res.status(200).json(putCategory.rows[0])
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500)
+    }
+})
+
+router.patch('/', async (req, res) => {
+    if (typeof req.body.id !== 'number') return res.sendStatus(400)
+
+    try {
+        const specificCategory = await pool.getInstance().query('SELECT * FROM kategoria_prawa_jazdy WHERE id = $1', [req.body.id])
+
+        if (specificCategory.rows.length === 0) return res.sendStatus(404)
+
+        const { id, kategoria } = {...specificCategory.rows[0], ...req.body}
+
+        const updatedCategory = await pool.getInstance().query('UPDATE kategoria_prawa_jazdy SET kategoria = $1 WHERE id = $2 RETURNING *',
+        [kategoria, id])
+
+        return res.status(200).json(updatedCategory.rows[0])
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500)
+    }
+})
+
+module.exports = router
