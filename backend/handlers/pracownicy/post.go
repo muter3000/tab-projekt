e0acf9691d5c1012cda78b6d6e2f8f6fd2f2547f
szebestik@gmail.com <szebestik@gmail.com>
2022-05-01 10:28:46 +0200

    Added paramaters validation for the post method for the pracownik table

--- backend/handlers/pracownicy/post.go
+++ backend/handlers/pracownicy/post.go
@@ -2,16 +2,22 @@ package pracownicy
 
 import (
 	"encoding/json"
-	"github.com/tab-projekt-backend/schemas"
-	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+
+	"github.com/tab-projekt-backend/schemas"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (p *Pracownicy) createNew(rw http.ResponseWriter, r *http.Request) {
 	p.l.Debug("handling post request", "path", p.path)
 	pracownik := schemas.Pracownik{}
 	err := json.NewDecoder(r.Body).Decode(&pracownik)
+	if pracownik.Haslo == "" || pracownik.Login == "" || pracownik.Imie == "" || pracownik.Nazwisko == "" ||
+		pracownik.Pesel == "" || len(pracownik.Pesel) != 11 {
+		http.Error(rw, "Wrong parameters passed", http.StatusBadRequest)
+		return
+	}
 	if err != nil {
 		p.l.Error("marshaling", "err", err)
 		http.Error(rw, "Creating new pracownik", http.StatusBadRequest)
