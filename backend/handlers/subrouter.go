24e9922b863c4d73cfae391826ed2f6326558895
muter3000 <muter3000@gmail.com>
2022-05-02 21:34:20 +0200

    Remove leftovers from postgres

--- backend/handlers/subrouter.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "github.com/gorilla/mux"
-
-type SubRouter interface {
-	// RegisterSubRouter Funkcja rejestruje subrouter dla podanego routera
-	RegisterSubRouter(router *mux.Router)
-}
