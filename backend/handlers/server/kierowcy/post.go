3bd69063dceed1694e5ec44bf7d712420890f887
szebestik@gmail.com <szebestik@gmail.com>
2022-05-15 12:36:25 +0200

    Transactions

--- backend/handlers/server/kierowcy/post.go
+++ backend/handlers/server/kierowcy/post.go
@@ -16,10 +16,15 @@ func (k *Kierowcy) createNew(rw http.ResponseWriter, r *http.Request) {
 		Haslo     string  `json:"haslo,omitempty"`
 		Kategorie []int32 `json:"kategorie"`
 	}
+
+	tx, err := k.db.Begin()
+	// Make sure to close transaction if something goes wrong.
+	defer tx.Close()
+
 	rw.Header().Add("Content-Type", "application/json")
 	k.l.Debug("handling post request", "path", k.path)
 	data := postData{}
-	err := json.NewDecoder(r.Body).Decode(&data)
+	err = json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		k.l.Error("marshaling", "err", err)
 		http.Error(rw, "Creating new kierowca", http.StatusInternalServerError)
@@ -28,16 +33,28 @@ func (k *Kierowcy) createNew(rw http.ResponseWriter, r *http.Request) {
 	kierowca := schemas.Kierowca{Pracownik: schemas.Pracownik{Pesel: data.Pesel, Imie: data.Imie,
 		Nazwisko: data.Nazwisko, Login: data.Login, Haslo: data.Haslo}}
 
-	_, err = k.db.Model(&kierowca).Returning("*", &kierowca).Insert()
+	_, err = tx.Model(&kierowca).Returning("*", &kierowca).Insert()
 	if err != nil {
-		k.l.Error("marshaling", "err", err)
+		k.l.Error("inserting kierowca", "err", err)
 		http.Error(rw, "Inserting into database", http.StatusInternalServerError)
+		_ = tx.Rollback()
 		return
 	}
 
 	for _, element := range data.Kategorie {
 		kategoria_kierowcy := schemas.KategoriaKierowcy{KierowcaId: kierowca.KierowcaID, KategoriaPrawaJazdyId: element}
-		k.db.Model(&kategoria_kierowcy).Insert()
+		_, err = tx.Model(&kategoria_kierowcy).Insert()
+
+		if err != nil {
+			k.l.Error("inserting kategorie", "err", err)
+			http.Error(rw, "Inserting into database", http.StatusInternalServerError)
+			_ = tx.Rollback()
+			return
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		panic(err)
 	}
 
 	err = json.NewEncoder(rw).Encode(kierowca)
